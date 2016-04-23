bcda7d0eddd5ab2cf9f157c98f9533c803769b91
ilife5 <ilife5@qq.com>
2016-04-23 15:04:30 +0800

    update

--- conf.js
+++ conf.js
@@ -1,8 +1,8 @@
 module.exports = {
     mailgun: {
-        api_key: "key-c472b61dcc81b411fa4475a33d49d2da",
-        domain: "cnts.help",
-        from: 'cnts <cnts@cnts.help>',
-        to: 'jifeng.yao@jimu.com'
+        api_key: "***",
+        domain: "***",
+        from: '***',
+        to: '***'
     }
 };
