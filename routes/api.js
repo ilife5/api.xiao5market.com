76c6f3129ced0d3641b2a3e8f79b4ffd432f9f6f
jifeng.yao <jifeng.yao@jimubox.com>
2016-12-01 17:10:28 +0800

    feature: update url

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ router.get('/jsConfig', function(req, res, next) {
   api.getJsConfig({
     debug: true,
     jsApiList: ['onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','onMenuShareQZone', 'hideAllNonBaseMenuItem', 'closeWindow', 'showMenuItems', 'hideOptionMenu', 'onMenuShareTimeline', 'hideMenuItems'],
-    url: 'http://www.xiao5market.com/'
+    url: encodeURIComponent(req.query.url)
   }, function(s, config) {
     res.jsonp(config);
   });
