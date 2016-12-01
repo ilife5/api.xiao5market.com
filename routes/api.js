cd88899b67d28d60f2063f16fde6ab8a66b8373c
jifeng.yao <jifeng.yao@jimubox.com>
2016-12-01 16:51:54 +0800

    feature :update api

--- routes/api.js
+++ routes/api.js
@@ -3,15 +3,14 @@ var router = express.Router();
 var WechatAPI = require('wechat-api');
 var api = new WechatAPI('wx188f53f66a0ff887', '37bf14bcbb426120e93a7e57c48587d0');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* GET users listing. */
+router.get('/jsConfig', function(req, res, next) {
   api.getJsConfig({
     debug: true,
     jsApiList: ['onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','onMenuShareQZone', 'hideAllNonBaseMenuItem', 'closeWindow', 'showMenuItems', 'hideOptionMenu', 'onMenuShareTimeline', 'hideMenuItems'],
     url: 'http://www.xiao5market.com/'
   }, function(s, config) {
-    console.log(arguments);
-    res.render('index', { title: 'Express', config: config});
+    res.jsonp(config);
   });
 });
 
