188696be65d5ce25ce87551f9c77c501293bc52c
jifeng.yao <jifeng.yao@jimubox.com>
2016-12-01 18:08:17 +0800

    feature: add k

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var WechatAPI = require('wechat-api');
 var api = new WechatAPI('wx188f53f66a0ff887', '37bf14bcbb426120e93a7e57c48587d0');
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,14 +17,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/foo/bar', function(req, res, next) {
-  api.getJsConfig({
-    debug: true,
-    jsApiList: ['onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','onMenuShareQZone', 'hideAllNonBaseMenuItem', 'closeWindow', 'showMenuItems', 'hideOptionMenu', 'onMenuShareTimeline', 'hideMenuItems'],
-    url: 'http://api.xiao5market.com/foo/bar'
-  }, function(s, config) {
-    console.log(arguments);
-    res.render('index', { title: 'Express', config: config});
+  request('http://api.xiao5market.com/api/jsConfig?url=' + encodeURIComponent("http://api.xiao5market.com/foo/bar"), function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+      res.render('index', { title: 'Express', config: eval('(' + body + ')')});
+    } else {
+      console.log(error);
+    }
   });
+
 });
 
 router.get('/MP_verify_qPgPGm52uZblZ9wq.txt', function(req, res) {
